b63b6afd3ad7e3e172fe1e4df20dc591ecd3c32d
FixOdoo <89758941+FixOdoo@users.noreply.github.com>
2021-09-08 16:53:03 +0800

    Create Sale Order Quotation Layout

---
+++ sale_order_layout/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Report Layout",
+    'summary': "",
+    'author': "FixOdoo",
+    'website': "https://fixodoo.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'sale', 'company_cop'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/reports.xml',
+        'views/sale_quotation.xml',
+    ]
+}
